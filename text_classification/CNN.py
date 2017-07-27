9f2978998b7e5129164f6ae34be2b580c5a20313
Little-girl-1992 <496385097@qq.com>
2017-07-27 15:35:38 +0800

    add CNN model in imdb dataset

---
+++ text_classification/CNN.py
@@ -0,0 +1,128 @@
+import numpy as np
+np.random.seed(1337)  # for reproducibility
+
+from keras.preprocessing import sequence
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation
+from keras.layers import Embedding
+from keras.layers import Conv1D, GlobalMaxPooling1D
+from keras.datasets import imdb
+from keras.optimizers import Adam
+
+from gensim.models import Word2Vec
+from gensim.models.keyedvectors import KeyedVectors
+
+class MyException(Exception):
+    def __init__(self,message):
+        Exception.__init__(self)
+        self.message=message
+
+def word_vector(key='word2vec', filename=''):
+    if filename == '':
+        raise MyException("filename is NUll")
+    if key == 'word2vec':
+        # word_vectors = KeyedVectors.load_word2vec_format(filename, binary=False)
+        model = Word2Vec.load(filename)
+        word_vectors = model.wv
+        print(len(word_vectors.vocab))
+        # del word_vectors
+        return word_vectors
+    
+    elif key == 'glove':
+        word_vectors = KeyedVectors.load_word2vec_format(filename, binary=False)
+        # print(word_vectors['of'])
+        # del word_vectors
+        return word_vectors
+    else:
+        raise MyException("key must be word2vec or glove")
+
+# set parameters:  设定参数
+max_features = 20000  # 最大特征数（词汇表大小）
+maxlen = 400         # 序列最大长度
+batch_size = 50      # 每批数据量大小
+embedding_dims = 50  # 词嵌入维度
+nb_filter = 250      # 1维卷积核个数
+filter_length = 5    # 卷积核长度
+hidden_dims = 250    # 隐藏层维度
+nb_epoch = 10        # 迭代次数
+
+# 载入 imdb 数据
+print('Loading data...')
+(X_train, y_train), (X_test, y_test) = imdb.load_data(path='/notebooks/my_word/data/imdb.npz',num_words=max_features)
+word_index = imdb.get_word_index(path='/notebooks/my_word/data/imdb_word_index.json')
+# print(len(word_index))
+print(len(X_train), 'train sequences')
+print(len(X_test), 'test sequences')
+
+
+# 样本填充到固定长度 maxlen，在每个样本前补 0
+print('Pad sequences (samples x time)')
+X_train = sequence.pad_sequences(X_train, maxlen=maxlen)
+X_test = sequence.pad_sequences(X_test, maxlen=maxlen)
+print('X_train shape:', X_train.shape)
+print('X_test shape:', X_test.shape)
+
+embedding_matrix = np.zeros((max_features, embedding_dims))
+embeddings_index = word_vector(key='word2vec',filename='/notebooks/my_word/CBOW/enwiki_1G_50d_iter10.bin')
+for word, i in word_index.items():
+    if int(i) < max_features:
+        # print(word.encode('UTF-8'),i)
+        if word in embeddings_index.vocab:
+            # print('find')
+            embedding_matrix[i] = embeddings_index[word]
+
+print('embedding_matrix:', np.shape(embedding_matrix))
+# 构建模型
+print('Build model...')
+model = Sequential()
+
+# we start off with an efficient embedding layer which maps
+# our vocab indices into embedding_dims dimensions
+# 先从一个高效的嵌入层开始，它将词汇的索引值映射为 embedding_dims 维度的词向量
+model.add(Embedding(max_features,
+                    embedding_dims,
+                    weights=[embedding_matrix],
+                    input_length=maxlen,
+                    trainable=False,
+                    dropout=0.3))
+
+# model.add(Embedding(max_features,
+#                     embedding_dims,
+#                     input_length=maxlen,
+#                     dropout=0.3))
+
+# we add a Convolution1D, which will learn nb_filter
+# word group filters of size filter_length:
+# 添加一个 1D 卷积层，它将学习 nb_filter 个 filter_length 大小的词组卷积核
+model.add(Conv1D(nb_filter=nb_filter,
+                        filter_length=filter_length,
+                        border_mode='valid',
+                        activation='relu',
+                        subsample_length=1))
+# we use max pooling:
+# 使用最大池化
+model.add(GlobalMaxPooling1D())
+
+# We add a vanilla hidden layer:
+# 添加一个原始隐藏层
+model.add(Dense(hidden_dims))
+model.add(Dropout(0.3))
+model.add(Activation('relu'))
+
+# We project onto a single unit output layer, and squash it with a sigmoid:
+# 投影到一个单神经元的输出层，并且使用 sigmoid 压缩它
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.summary()  # 模型概述
+
+# 定义损失函数，优化器，评估矩阵
+model.compile(loss='binary_crossentropy',
+              optimizer=Adam(lr=2e-4),
+              metrics=['accuracy'])
+
+# 训练，迭代 nb_epoch 次
+model.fit(X_train, y_train,
+          batch_size=batch_size,
+          epochs=nb_epoch,
+          validation_data=(X_test, y_test))
